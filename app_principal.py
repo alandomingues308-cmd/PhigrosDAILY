2768f4e33ff6d195b11fbdbbc2212165b5e21750
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-10 08:33:30 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -67,7 +67,7 @@ if uploaded_file is not None:
     
     with st.spinner("Analizando captura..."):
         # 📌 1. Área del Usuario (Esquina superior derecha)
-        box_usuario = (int(ancho * 0.68), int(alto * 0.02), int(ancho * 0.82), int(alto * 0.12))
+        box_usuario = (int(ancho * 0.60), int(alto * 0.02), int(ancho * 0.82), int(alto * 0.12))
         img_usuario = np.array(imagen_completa.crop(box_usuario))
         ocr_user = reader.readtext(img_usuario, detail=0)
         usuario_detectado = " ".join(ocr_user).strip() if ocr_user else "Usuario_Desconocido"
