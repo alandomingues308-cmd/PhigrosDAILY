30cad9e2999d845aaffd8d5e3c55a48b8345f6af
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-10 02:06:23 -0600

    Update subir_score.py

--- pages/subir_score.py
+++ pages/subir_score.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 import easyocr
 from PIL import Image
-from datetime import datetime
+import datetime
 
 from google.cloud import firestore
 from google.oauth2 import service_account
