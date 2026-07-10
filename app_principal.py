a8d19d64f4239936938ed624fb504008e72ae654
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-10 04:04:41 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -222,17 +222,17 @@ if st.button("Validar y Registrar Puntaje"):
              "rango": rango,
              "fecha": today
             }
-       
+       doc_id = f"{usuario_activo}_{cancion_objetivo}_{today}"
+       doc_ref = db.collection("scores").document(doc_id)
+       doc_ref.set(nuevo_score)
+                
+       st.success(f"🏆 ¡Score registrado en la nube! RKS final logrado: {rks_final} ({rango})")
+       st.balloons()
+
             
 
             # 🔥 GUARDAR DIRECTO EN LA BASE DE DATOS DE STREAMLIT
             # Crea un identificador único en la nube (Usuario_Cancion_Fecha) para no duplicar datos
-doc_id = f"{usuario_activo}_{cancion_objetivo}_{today}"
-doc_ref = db.collection("scores").document(doc_id)
-doc_ref.set(nuevo_score)
-                
-st.success(f"🏆 ¡Score registrado en la nube! RKS final logrado: {rks_final} ({rango})")
-st.balloons()
 
 
 # =============================================================================
