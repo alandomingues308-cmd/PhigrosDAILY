04248aac12a287410755252b4a5149a94c7073e0
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-10 12:10:26 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -130,9 +130,9 @@ if uploaded_file is not None:
         # Lógica de cálculo (reutilizando tus funciones previas)
             def calcular_rks(acc, const): return round((((acc - 55) / 45) ** 2) * const, 2)
         rks= calcular_rks(accuracy_detectada, constante_activa)
-
+        perdidas= bad_detectados + miss_detectados
         if score_detectado == 1000000: bono= 2
-        elif (bad_detectados + miss_detectados)== 0: bono= 1.5
+        elif perdidas== 0: bono= 1.5
         elif score_detectado >= 960000: bono= 1
         elif score_detectado >= 920000: bono= 0.5
         elif score_detectado >= 880000: bono= 0.2
