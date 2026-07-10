fdb5cab83b0fc0d81c3a5cdbf5ac68ebba38203c
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-10 12:26:29 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -100,22 +100,20 @@ if uploaded_file is not None:
                 break
 
         # 📌 5. Área BAD/MISS
-        def extraer_numero(img_crop):
-            res = reader.readtext(img_crop, detail=0, allowlist='0123456789')
-            for item in res:
-                if item.isdigit():
-                    return int(item)
-                    return 0
-        # Box Bad
-        box_bad = (int(ancho * 0.61), int(alto * 0.67), int(ancho * 0.66), int(alto * 0.77))
-        img_bad = imagen_completa.crop(box_bad)
-        bad = extraer_numero(np.array(img_bad))
-    
-        # Box Miss
-        box_miss = (int(ancho * 0.71), int(alto * 0.67), int(ancho * 0.76), int(alto * 0.77))
-        img_miss = imagen_completa.crop(box_miss)
-        miss = extraer_numero(np.array(img_miss))
+        for elemento in lineas_texto:
+        texto_limpio = elemento.strip()
+        # Buscamos una secuencia de números separados por espacios (ej: "1257 0 0 0")
+        numeros_linea = re.findall(r'\b\d+ \d+ \d+ \d+\b', texto_limpio)
         
+        if numeros_linea:
+            valores = [int(n) for n in numeros_linea[0].split()]
+            perfect_detectado = valores[0]
+            good_detectado = valores[1]
+            bad_detectado = valores[2]
+            miss_detectado = valores[3]
+            desglose_encontrado = True
+            break
+            
         #Los que se leyeron mal
         if usuario_final== "crafi": usuario_final= "craftyy!"
         if usuario_final== "Evz": usuario_final= "Evanii"
@@ -130,7 +128,7 @@ if uploaded_file is not None:
         if st.button("Registrar Puntaje"):
         # Lógica de cálculo (reutilizando tus funciones previas)
             def calcular_rks(acc, const): return round((((acc - 55) / 45) ** 2) * const, 2)
-        rks= calcular_rks(accuracy_detectada, constante_activa)
+            rks= calcular_rks(accuracy_detectada, constante_activa)
         perdidas= bad + miss
         
         if score_detectado == 1000000: bono= 2
