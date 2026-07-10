d718e969b1fdb9f3892da6d9d0c636fc18167a7a
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-10 17:10:59 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -125,6 +125,7 @@ if uploaded_file is not None:
         if usuario_final== ">OMathyPop": usuario_final= "MathyPop"
         if usuario_final== "MalenaF": usuario_final= "MalenaPop"
         if usuario_final== "5 MathyPop": usuario_final= "MathyPop"
+        usuario_detectado= st.text.input("usuario: ")
         
         # UI de confirmación
         st.subheader("📝 Datos Extraídos")
