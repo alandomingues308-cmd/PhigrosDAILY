eacfd3be1b70e3d7bd35eb35777f94bb4ecf1e6d
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-10 16:07:30 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -55,7 +55,7 @@ reader = inicializar_ocr()
 
 # Variables de estado
 cancion_objetivo = daily_song["title"]
-constante_activa = daily_song.get("IN") # Ajustar según dificultad si es necesario
+constante_activa = daily_song["IN"] # Ajustar según dificultad si es necesario
 
 st.title("🏆 Sube tu mejor puntaje")
 
