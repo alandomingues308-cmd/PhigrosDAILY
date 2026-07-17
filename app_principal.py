91ca41b655eac9f5a42ee8026751b434c323de79
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-16 20:22:20 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -242,6 +242,7 @@ with tab_arcaea:
     st.subheader("Cancion Alternativa")
     st.info(f"**{alternative_song_a['title']}**")
 
+    st.title("🏆 Sube tu mejor puntaje")
     usuario_final_a = st.text_input("Coloca tu usuario", placeholder="Tu nombre de usuario", key="ar_user")
 
     uploaded_file_a = st.file_uploader("Sube la captura de pantalla de tus resultados (Arcaea):", 
@@ -254,7 +255,6 @@ with tab_arcaea:
         with st.spinner("Analizando captura..."):
             score_detectado = 0
             strategies = [(0.22, 0.15, 0.80, 0.42), (0.28, 0.20, 0.75, 0.38), (0.18, 0.12, 0.85, 0.48)]
-            st.success(f"{daily_song_a['title']} FTR: {daily_song_a["FTR"]} ETR: {daily_song_a["ETR"]}")
             for left, top, right, bottom in strategies:
                 box = (int(ancho*left), int(alto*top), int(ancho*right), int(alto*bottom))
                 img = np.array(imagen_completa.crop(box))
