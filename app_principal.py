2c9fd321a16ed0a70e5e5d2a94058f5469a1e7a8
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-16 20:07:17 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -228,13 +228,13 @@ with tab_arcaea:
 
     if daily_song_a["ETR"] is not None:
         st.success(f"{daily_song_a['title']}/ (FTR: {daily_song_a["FTR"]}) - (ETR: {daily_song_a["ETR"]})")
-    elif daily_song_a["beyond"] is not None:
+    elif daily_song_a["Beyond"] is not None:
         st.success(f"{daily_song_a['title']} FTR: {daily_song_a["FTR"]} BYD: {daily_song_a["beyond"]}")
     else:st.success(f"{daily_song_a['title']} FTR: {daily_song_a["FTR"]}")
 
     if alternative_song_a["ETR"] is not None:
         st.success(f"{alternative_song_a['title']}/ (FTR: {alternative_song_a["FTR"]}) - (ETR: {alternative_song_a["ETR"]})")
-    elif alternative_song_a["beyond"] is not None:
+    elif alternative_song_a["Beyond"] is not None:
         st.success(f"{alternative_song_a['title']} FTR: {alternative_song_a["FTR"]} BYD: {alternative_song_a["beyond"]}")
     else:st.success(f"{alternative_song_a['title']} FTR: {alternative_song_a["FTR"]}")
 
