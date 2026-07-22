74808deba32c531142f475ce47b7ea3a533b5da0
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-21 22:22:29 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -75,7 +75,7 @@ with tab_phigros:
     CANCION_DAILY = daily_song["title"]
     CANCION_ALT = alternative_song["title"]
 
-    st.success(f"{CANCION_DAILY} ({daily_song.get('IN')} / (16.2))")
+    st.success(f"{CANCION_DAILY} ({daily_song.get('IN')}) / (16.2)")
     st.subheader("Cancion Alternativa")
     st.info(f"**{CANCION_ALT} ({alternative_song.get('IN', '')})**")
 
