a792957cb34f9d5e4496b0ee3e7d9ae58655fcfb
alandomingues308-cmd <alandomingues308@gmail.com>
2026-07-30 22:12:09 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -78,7 +78,7 @@ with tab_phigros:
 
     st.success(f"{CANCION_DAILY} ({daily_song.get('IN')})")
     st.subheader("Cancion Alternativa")
-    st.info(f"**{CANCION_ALT} ({alternative_song.get('IN', '')}) **")
+    st.info(f"**{CANCION_ALT} ({alternative_song.get('IN', '')}) / (15.9) **")
 
 
     @st.cache_resource
