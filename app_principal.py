805d97761d51272df1c60860e70e13e4a19a098f
alandomingues308-cmd <alandomingues308@gmail.com>
2026-08-07 22:17:54 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -80,6 +80,10 @@ with tab_phigros:
     else: st.success(f"{CANCION_DAILY} IN: ({daily_song.get('IN')})")
     
     st.subheader("Cancion Alternativa")
+
+    if alternative_song["AT"] is not None:
+        st.info(f"{CANCION_ALT} IN: ({alternative_song.get('IN')}) / AT: ({alternative_song.get('AT')})")
+    else: st.info(f"{CANCION_ALT} IN: ({alternative_song.get('IN')})")
     
     st.info(f"**{CANCION_ALT} ({alternative_song.get('IN', '')})**")
 
