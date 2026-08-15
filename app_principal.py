6120ecfe02bc22b64e9e8d5f6304f1f73f70fa1e
alandomingues308-cmd <alandomingues308@gmail.com>
2026-08-14 23:44:11 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -12,6 +12,7 @@ import pytz
 import random
 import json
 import rosu_pp_py as rosu
+import request 
 
 # ====================== CONFIGURACIÓN ======================
 st.set_page_config(page_title="Daily Challenge", layout="wide")
