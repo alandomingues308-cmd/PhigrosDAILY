da56986f1fb33e7a7e183eb1cf89b7cf584f54f8
alandomingues308-cmd <alandomingues308@gmail.com>
2026-08-14 23:45:41 -0600

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app_principal.py
+++ app_principal.py
@@ -12,7 +12,7 @@ import pytz
 import random
 import json
 import rosu_pp_py as rosu
-import requesst 
+import requests 
 
 # ====================== CONFIGURACIÓN ======================
 st.set_page_config(page_title="Daily Challenge", layout="wide")
