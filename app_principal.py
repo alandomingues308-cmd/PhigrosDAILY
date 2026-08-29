1b6a2fa3650f0e7f9b1da94c51d8408d7531b87e
alandomingues308-cmd <alandomingues308@gmail.com>
2026-08-28 20:27:59 -0600

    Update app_principal.py

--- app_principal.py
+++ app_principal.py
@@ -364,7 +364,7 @@ with tab_arcaea:
         st.success(f"{daily_song_a['title']} /// (FTR: {daily_song_a["FTR"]}) - (ETR: {daily_song_a["ETR"]})")
     elif daily_song_a["BYD"] is not None:
         st.success(f"{daily_song_a['title']} /// (FTR: {daily_song_a["FTR"]}) - (BYD: {daily_song_a["BYD"]})")
-    elif daily_song_a["INS"] is not None:
+    elif daily_song_a.get["INS"] is not None:
         st.info(f"{daily_song_a['title']} /// (FTR: {daily_song_a["FTR"]}) - (INS: {daily_song_a["INS"]})")
     else:st.success(f"{daily_song_a['title']} /// (FTR: {daily_song_a["FTR"]})")
 
